6abd111258353d8c2cab9dc8c49d708f9b51ce86
litao <litao@intelth.com.cn>
2022-04-28 17:10:45 +0800

    修改打包方式，防止单体文件太大

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
@@ -21,7 +21,40 @@ export default defineConfig({
         // 配置路径别名
         alias: {
             "@": path.resolve(__dirname, "./src"),
-            "@api": path.resolve(__dirname, "./src/request/apis")
+            "@api": path.resolve(__dirname, "./src/request/apis"),
+        },
+    },
+    //分包打包，防止单体文件太大
+    build: {
+        sourcemap: false,
+        minify: "terser",
+        chunkSizeWarningLimit: 1500,
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+            },
+        },
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return id
+                            .toString()
+                            .split("node_modules/")[1]
+                            .split("/")[0]
+                            .toString();
+                    }
+                },
+                chunkFileNames: (chunkInfo) => {
+                    const facadeModuleId = chunkInfo.facadeModuleId
+                        ? chunkInfo.facadeModuleId.split("/")
+                        : [];
+                    const fileName =
+                        facadeModuleId[facadeModuleId.length - 2] || "[name]";
+                    return `js/${fileName}/[name].[hash].js`;
+                },
+            },
         },
     },
 });
